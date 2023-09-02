85139a937b785fd9984b3df57d118af3359376a8
Neosaber <gdidenis@mail.ru>
2023-09-02 15:16:57 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@
 
 importScripts('sw-toolbox.js');
 
-toolbox.precache(["index.html","style/style.css"]);
+toolbox.precache(["kidsfm/index.html","kidsfm/style/style.css"]);
 
-toolbox.router.get('/images/*', toolbox.cacheFirst);
+toolbox.router.get('/kidsfm/images/*', toolbox.cacheFirst);
 
 toolbox.router.get('/*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
